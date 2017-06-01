a73e05e36182ba1cd18d68938a18a4207dfa4105
Nyma Dolatkhahnejad <mustachedeliva@gmail.com>
2017-06-01 12:04:28 +0200

    Added footer, changed some  code

--- index.php
+++ index.php
@@ -1,32 +1,31 @@
 <?php
 session_start();
+
 if (isset($_GET['page'])) {
-        $page = $_GET['page'];
+    $page = $_GET['page'];
 } else {
-    $page='home';
+    $page= "home";
 }
 ?>
 
 <head>
     <title/>Index</title>
-    <!-- Bootstrap -->
     <link href="css/style.css" rel="stylesheet">
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
 </head>
     <body>
-    <?php include 'includes/navbar.inc.php'?>
-    <div class="jumbotron">
-    <?php include 'includes/'.$page.'.inc.php'?>
+        <?php include 'includes/navbar.inc.php'?>
+        <div class="jumbotron">
+        <?php include 'includes/'.$page.'.inc.php'?>
         </div>
+
+        <footer class="footer">
+            BookOnShelf version 2.0 - Patrick Coughing
+        </footer>
     </body>
 
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="js/bootstrap.min.js"></script>
 </html>
--- private/connection.php
+++ private/connection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "poep";
+$password = "";
 $dbname = "bookonshelf";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
