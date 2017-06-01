e2c29dbf861534dec3da3b78cc9f46620e480516
Jurgen Klomp <jurgenklomp883@gmail.com>
2017-06-01 20:59:35 +0200

    Small fixes

--- index.php
+++ index.php
@@ -10,11 +10,13 @@ if (isset($_GET['page'])) {
 
 <head>
     <title/>Index</title>
+    <link rel="icon" href="images/favicon.ico">
     <link href="css/style.css" rel="stylesheet">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 </head>
+
     <body>
         <?php include 'includes/navbar.inc.php'?>
         <div class="jumbotron">
