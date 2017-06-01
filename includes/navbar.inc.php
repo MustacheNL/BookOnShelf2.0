ab8a3a1f4114659779e5e4846a43ef8f1a18fa8e
Jurgen Klomp <jurgenklomp883@gmail.com>
2017-06-01 12:04:33 +0200

    fixed navbar

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -1,9 +1,11 @@
+<div class="nav">
 <ul>
     <li><a href="index.php?page=home">Home</a></li>
     <li><a href="index.php?page=boekenlijst">Boekenlijst</a></li>
     <li><a href="index.php?page=boektoevoegen">Boek toevoegen</a></li>
     <li><a href="index.php?page=contact">Contact</a></li>
-    <li><a href="index.php?page=settings">Settings</a></li>
-    <li><a href="index.php?page=dashboard">Dashboard</a></li>
+    <li><a href="index.php?page=settings">Account instellingen</a></li>
+    <li><a href="index.php?page=dashboard">Administratie paneel</a></li>
 
 </ul>
+</div>
