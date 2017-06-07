fb9ed1898e5bb915483322cfa8b66b36a18dd1d0
Nyma Dolatkhahnejad <mustachedeliva@gmail.com>
2017-06-07 09:27:44 +0200

    Cleaned code, changed some make up shit!

--- php/hirebook.php
+++ php/hirebook.php
@@ -1,9 +1,12 @@
 <?php
 include('../private/connection.php');
+include('../class/class.user.php');
+$auth_user = new USER();
 session_start();
+
 /* Kijkt of de GET hire bevat en zet het boeken op gehuurd */
     $id = $_GET['hire'];
-    $stmt = $conn->prepare("UPDATE `books` SET `hired` = '1', hired_date = CURRENT_DATE WHERE `id` = $id");
+    $stmt = $auth_user->runQuery("UPDATE `books` SET `hired` = '1', hired_date = CURRENT_DATE WHERE `id` = $id");
     $stmt->execute();
     header('location: ../index.php?page=booklist&msg=success');
 ?>
