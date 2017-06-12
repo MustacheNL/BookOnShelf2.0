92601915bdba777adc3124f1bac5344d347d1f23
Nyma Dolatkhahnejad <mustachedeliva@gmail.com>
2017-06-12 09:26:37 +0200

    Cleaned code, added more comments. Made check for user session. Fixed register. Started with settings page.

--- includes/settings.inc.php
+++ includes/settings.inc.php
@@ -30,10 +30,89 @@ if (isset($_POST['btn-submit'])) {
         $error[] = "Je huisnummer moet wel nummers bevatten!";
     }/* elseif() {
         $error[] = "";
-    } */else {
+    } */ else {
         $stmt = $auth_user->runQuery("UPDATE users SET `street` = :street, `housenumber` = :housenumber, `housenumber_ext` = :housenumber_ext, `zipcode` = :zipcode, `city` = :city, `country` = :country, `gender` = :gender WHERE `user_id` = :user_id");
         $stmt->execute(array(':street' => $_POST['txt_street'], ':housenumber' => $_POST['txt_housenumber'], ':housenumber_ext' => $_POST['txt_housenumber_ext'], ':zipcode' => $_POST['txt_zipcode'], ':city' => $_POST['txt_city'], ':country' => $_POST['txt_country'], ':gender' => $_POST['txt_gender'], ':user_id' => $user_id));
-        header("Location: /settings.php");
+        header("Location: index.php?page=settings");
     }
 }
 ?>
+
+<main class="mdl-layout__content mdl-color--grey-100" style="display: block;">
+    <div class="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid"
+         style="margin-top: 50px">
+        <form method="post" class="form-signin">
+            <?php
+            if (isset($error)) {
+                foreach ($error as $error) {
+                    ?>
+                    <span class="mdl-chip mdl-chip--contact">
+                                <span class="mdl-chip__contact mdl-color--red mdl-color-text--white">!</span>
+                                <span class="mdl-chip__text"><?php echo $error; ?></span>
+                            </span>
+                    <?php
+                }
+            } ?>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                <input class="mdl-textfield__input" type="text" id="sample1" name="txt_street"
+                       value="<?php echo $street; ?>">
+                <label class="mdl-textfield__label" for="sample1">Straat</label>
+            </div>
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label"
+                     style="width 35%; float: left; overflow: hidden;">
+                    <input class="mdl-textfield__input" type="text" id="sample1"
+                           name="txt_housenumber"
+                           value="<?php echo $housenumber; ?>">
+                    <label class="mdl-textfield__label" for="sample1">Huisnummer</label>
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                    <input class="mdl-textfield__input" type="text" id="sample1" name="txt_housenumber_ext"
+                           value="<?php echo $housenumber_ext; ?>">
+                    <label class="mdl-textfield__label" for="sample1">Toevoeging</label>
+                </div>
+            </div>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                <input class="mdl-textfield__input" type="text" id="sample1" name="txt_zipcode"
+                       value="<?php echo $zipcode; ?>">
+                <label class="mdl-textfield__label" for="sample1">Postcode</label>
+            </div>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                <input class="mdl-textfield__input" type="text" id="sample1" name="txt_city"
+                       value="<?php echo $city; ?>">
+                <label class="mdl-textfield__label" for="sample1">Stad</label>
+            </div>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                <input class="mdl-textfield__input" type="text" id="sample1" name="txt_country"
+                       value="<?php echo $country; ?>">
+                <label class="mdl-textfield__label" for="sample1">Land</label>
+            </div>
+            Gender
+            <select name="txt_gender">
+
+                <option <?php if ($gender === "0") {
+                    echo "selected='selected'";
+                } ?> value="0">Man
+                </option>
+                <option <?php if ($gender === "1") {
+                    echo "selected='selected'";
+                } ?> value="1">Vrouw
+                </option>
+                <option <?php if ($gender === "2") {
+                    echo "selected='selected'";
+                } ?> value="2">Apache Helicopter
+                </option>
+                <option <?php if ($gender === "3") {
+                    echo "selected='selected'";
+                } ?> value="3">Privé
+                </option>
+            </select>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                <input class="mdl-textfield__input" type="text" id="sample1" disabled name="txt_register"
+                       value="<?php echo $registerdate; ?>">
+                <label class="mdl-textfield__label" for="sample1">Registratiedatum</label>
+            </div>
+            <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored,  btn-primary"
+                    type="submit" name="btn-submit">
+                <i class="glyphicon glyphicon-open-file"></i>Submit
+            </button>
+        </form>
+    </div>
